10ff30fac99cc096353ec2c98c486e837dcd3f8c
Cerchie <luciacerchie@gmail.com>
2024-09-01 08:52:59 -0700

    more styles

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/services">Services</Link></li>
-            <li><Link href="/contact">Contact Form</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
           </ul>
         </nav>
       </header>
