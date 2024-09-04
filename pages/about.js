35790ba5eadb7a45de613a8b47156a1f8f3d215c
Cerchie <luciacerchie@gmail.com>
2024-09-03 18:26:58 -0700

    apostrophe lint issue

--- pages/about.js
+++ pages/about.js
@@ -9,15 +9,15 @@ export default function About() {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/services">Services</Link></li>
-            <li><Link href="https://docs.google.com/forms/d/e/1FAIpQLSfex5d6buKZulakrUFSUvXL1Mym1TmOJ_mths5KsMgHpJq0-A/viewform">Contact Form</Link></li>
+            <li><Link href="https://docs.google.com/forms/d/e/1FAIpQLSfex5d6buKZulakrUFSUvXL1Mym1TmOJ_mths5KsMgHpJq0-A/viewform">Contact</Link></li>
           </ul>
         </nav>
       </header>
       <main>
         <h1>About Taylor Cerchie</h1>
-        <p class="about-para">Taylor Cerchie is exceptionally qualified to provide data-minded business consulting, drawing from a robust foundation of five years of experience as a finance analyst and a strong academic background with a master's degree in mechanical engineering from Arizona State University. </p>
+        <p class="about-para">Taylor Cerchie is exceptionally qualified to provide data-minded business consulting, drawing from a robust foundation of five years of experience as a finance analyst and a strong academic background with a master&apos;s degree in mechanical engineering from Arizona State University. </p>
         <p class="about-para"> His analytical skills are further enhanced by his Excel wizardry, enabling him to efficiently manage, interpret, and visualize complex data sets.</p>
-        <p class="about-para"> Taylor’s unique combination of engineering problem-solving skills and financial expertise allows him to approach business challenges with a methodical, data-driven mindset, ensuring that his consulting advice is both strategically sound and practically implementable.</p>
+        <p class="about-para"> Taylor&apos;s unique combination of engineering problem-solving skills and financial expertise allows him to approach business challenges with a methodical, data-driven mindset, ensuring that his consulting advice is both strategically sound and practically implementable.</p>
       </main>
       <footer>
         <p id="disclaimer-about">Disclaimers: DataMinded consulting does not provide services exclusively provided by CPAs or CFPs.</p>
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function Home() {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/services">Services</Link></li>
-            <li><Link href="/contact">Contact Form</Link></li>
+            <li><Link href="https://docs.google.com/forms/d/e/1FAIpQLSfex5d6buKZulakrUFSUvXL1Mym1TmOJ_mths5KsMgHpJq0-A/viewform">Contact</Link></li>
           </ul>
         </nav>
       </header>
       <main>
         <h1>DataMinded Strategies</h1>
-        <p>Does your business need a little TLC? Treat it to some Tailored Local Consulting! DataMinded serves businesses in the greater Flagstaff area. <Link href="/contact">Contact Taylor Lee Cerchie</Link> today for a free consultation.</p>
+        <p>Does your business need a little TLC? Treat it to some Tailored Local Consulting! DataMinded serves businesses in the greater Flagstaff area. <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfex5d6buKZulakrUFSUvXL1Mym1TmOJ_mths5KsMgHpJq0-A/viewform">Contact Taylor Lee Cerchie</Link> today for a free consultation.</p>
         <img src="/logo.png" alt="just an image" />
       </main>
       <footer>
