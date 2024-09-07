50dd1b67b2a37ac87d8bd5ee4ab38ed4fe697d4e
Cerchie <luciacerchie@gmail.com>
2024-09-07 12:03:55 -0700

    style update

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="viewport-fit=cover" />
         <style>
-@import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        <style>
+@import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&family=SUSE:wght@100..800&display=swap');
+</style>
 </style>
       </Head>
       <body>
